8825ce5acb3f9953c0c94bf572998300ef444533
chorgan93 <chorgan93@gmail.com>
2014-09-22 21:15:12 -0700

    FIRE
    Got some temp fire stuff in to prep for getting fire meta implemented.

---
+++ Assets/__Scripts/EnvironmentalS/FireS.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireS : MonoBehaviour {
+
+	public List<Texture>	animationFrames;
+	public float changeFrameTime = 0.3f;
+	public float changeFrameCountdown;
+	public int currentFrame;
+
+	public float lifeTime = 3;
+	
+	void FixedUpdate () {
+
+		AnimateFire();
+		FireDecay();
+	
+	}
+
+	void FireDecay (){
+
+		lifeTime -= Time.deltaTime;
+		if (lifeTime <= 0){
+			Destroy(gameObject);
+		}
+
+	}
+
+	void AnimateFire () {
+
+		changeFrameCountdown -= Time.deltaTime;
+		if (changeFrameCountdown <= 0){
+
+			currentFrame ++;
+			if (currentFrame > animationFrames.Count - 1){
+				currentFrame = 0;
+			}
+
+			renderer.material.SetTexture("_MainTex", animationFrames[currentFrame]);
+
+			changeFrameCountdown = changeFrameTime;
+
+		}
+
+	}
+
+	void OnTriggerStay (Collider other) {
+
+		if (other.gameObject.tag == "Enemy"){
+			if (other.gameObject.GetComponent<EnemyS>().canBeSetOnFire){
+				other.gameObject.GetComponent<EnemyS>().onFire = true;
+			}
+		}
+
+	}
+}
---
+++ Assets/__Scripts/_PlayerScripts/MetaScripts/FireMetaS.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireMetaS : MetaGeneralS {
+
+	public GameObject	firePrefab;
+	public GameObject	fireBallPrefab;
+
+	public float 		fireBallSpeed;
+	public float 		fireBallRateOfFire = 2;
+	public float 		fireBallROFCountdown;
+	public float 		fireBallAccMult = 1;
+
+	public float 		initialFireSpawnRad = 3;
+	public int			numFireToSpawnInitial = 6;
+	public bool 		spawnedInitialFire = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		attachedHowie = gameObject.GetComponent<HowieS>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (activated){
+			UpdateMeta();
+			FireBallAttack();
+			if (!spawnedInitialFire){
+				ActivateFire();
+			}
+		}
+		else{
+			spawnedInitialFire = false;
+		}
+	
+	}
+
+	void FireBallAttack (){
+
+		if (fireBallROFCountdown > 0){
+			fireBallROFCountdown -= Time.deltaTime;
+		}
+		else{
+
+			string[] checkInputs = Input.GetJoystickNames();
+			int numInputs = checkInputs.Length;
+
+			if (numInputs > 0){
+
+				if (Application.platform == RuntimePlatform.OSXEditor || 
+				    Application.platform == RuntimePlatform.OSXPlayer ||
+				    Application.platform == RuntimePlatform.OSXWebPlayer || 
+				    Application.platform == RuntimePlatform.OSXDashboardPlayer){
+
+				}
+
+			}
+			else{
+
+			}
+
+		}
+
+	}
+
+	void ActivateFire(){
+
+		for (int i = 0; i < numFireToSpawnInitial; i++){
+			print ("GO");
+
+			Vector3 spawnFirePos = attachedHowie.transform.position;
+			spawnFirePos.z += 1;
+			spawnFirePos.x += Random.insideUnitCircle.x*initialFireSpawnRad;
+			spawnFirePos.y += Random.insideUnitCircle.y*initialFireSpawnRad;
+
+			Instantiate(firePrefab,spawnFirePos,Quaternion.identity);
+
+		}
+
+		spawnedInitialFire = true;
+
+	}
+}
