3b415117d65589e8f7b611636703264d9798b7e1
Steven Francus <stevenfrancus@Stevens-MacBook-Pro.local>
2014-09-25 18:11:17 -0700

    Added Event Handlers, and NPC Logic

---
+++ Assets/__Scripts/NPCActiveRadiusS.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCActiveRadiusS : MonoBehaviour {
+
+	// place this on sphere collider around enemies who need to detect Howie or other copies of itself
+	
+	public bool	howieIsInRange;
+	public NPCS parent;
+	public NPCActiveRadiusS() {
+		parent = transform.parent.gameObject.GetComponent<NPCS>();
+	}
+	void OnTriggerEnter (Collider other){
+		howieIsInRange = EventHandlers.checkIfPlayerCollision (other);
+	}
+	void Update() {
+		if (howieIsInRange && EventHandlers.shouldTalk()) {
+			parent.speak();
+		}
+	}
+
+
+	void OnTriggerExit (Collider other){
+		howieIsInRange = EventHandlers.checkIfPlayerCollision (other);
+	}
+}
