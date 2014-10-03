bb8886ab864eb449f1fcc1b0d62aa00998fc315b
Steven Francus <stevenfrancus@Stevens-MacBook-Pro.local>
2014-10-02 17:31:59 -0700

    So hot right now

--- Assets/__Scripts/_PlayerScripts/MetaScripts/MetaGeneralS.cs
+++ Assets/__Scripts/_PlayerScripts/MetaScripts/MetaGeneralS.cs
@@ -31,5 +31,4 @@ public class MetaGeneralS : MonoBehaviour {
 		activated = true;
 
 	}
-
 }
--- Assets/__Scripts/_PlayerScripts/MetaScripts/MetaS.cs
+++ Assets/__Scripts/_PlayerScripts/MetaScripts/MetaS.cs
@@ -4,41 +4,27 @@ using System.Timers;
 
 public abstract class MetaS : MonoBehaviour
 {	
-	public float DURATION_MAX;
-
 	public HowieCtrl HowieController;
 	public YarlaCtrl YarlaController;
 	public ChompCtrl ChompController;
-	public float duration;
+	public double Duration;
 	public Timer timer;
 
 	public MetaS ()
 	{
 		
 	}
-
-	//Every metamorphosis should call this super constructor to pass its set of controllers
+	
 	public MetaS(HowieCtrl howieCtrl, YarlaCtrl yarlaCtrl, ChompCtrl chompCtrl)
 	{
 		HowieController = howieCtrl;
 		YarlaController = yarlaCtrl;
 		ChompController = chompCtrl;
 	}
-
-
-	//ABSTRACT METHODS
-
-	//Every metamorphosis should be able to return its cost for each energy type
-	public abstract int getBlueCost(); 
-	public abstract int getRedCost(); 
-	public abstract int getPurpleCost(); 
-
-	//Every metamorphosis should be able to check if it is available based on Howie's energy values
+		
 	public abstract bool isAllowed(int redEnergy, int blueEnergy, int purpleEnergy);
 
-	//Every metamorphosis should have this method to properly set all attributes, load controllers, etc.
 	public abstract void activate(HowieS howie, YarlaS yarla, NewChompS chomp);
 
-	//Every metamorphosis should be able to "clean up after itself" after it ends
 	public abstract void deactivate();
 }
